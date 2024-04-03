324d6fc47638a697e1ecb6df0fc9957303eb5e46
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-03 21:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let modal = document.querySelector(".modal");
+let openModalButton = document.getElementById("openModal");
+
+openModalButton.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+let closeModalButton = document.querySelector(".close-modal");
+
+closeModalButton.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
